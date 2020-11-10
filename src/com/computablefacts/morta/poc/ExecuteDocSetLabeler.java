f4b6acaf0ab3a75e08070c000d8b815029a8bc37
Cyrille Savelief <csavelief@mncc.fr>
2020-11-10 11:16:26 +0100

    Prepare backport of DocSetLabeler command line.

---
+++ src/com/computablefacts/morta/poc/ExecuteDocSetLabeler.java
@@ -0,0 +1,10 @@
+package com.computablefacts.morta.poc;
+
+import com.computablefacts.nona.helpers.CommandLine;
+
+final public class ExecuteDocSetLabeler extends CommandLine {
+
+  public static void main(String[] args) {
+    // TODO
+  }
+}
