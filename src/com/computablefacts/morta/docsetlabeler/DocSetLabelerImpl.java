328e8c5be1034b83c44cca36358d610a4381bc33
Cyrille Savelief <csavelief@mncc.fr>
2021-04-30 19:42:12 +0200

    Performance improvements.

--- src/com/computablefacts/morta/docsetlabeler/DocSetLabelerImpl.java
+++ src/com/computablefacts/morta/docsetlabeler/DocSetLabelerImpl.java
@@ -1,7 +1,7 @@
 package com.computablefacts.morta.docsetlabeler;
 
-import java.util.AbstractMap;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -14,16 +14,12 @@ import org.tartarus.snowball.SnowballStemmer;
 
 import com.computablefacts.morta.snorkel.Helpers;
 import com.computablefacts.nona.helpers.AsciiProgressBar;
-import com.computablefacts.nona.helpers.BagOfTexts;
 import com.computablefacts.nona.helpers.DocSetLabeler;
-import com.computablefacts.nona.helpers.IBagOfTexts;
 import com.computablefacts.nona.helpers.Languages;
-import com.computablefacts.nona.helpers.Strings;
-import com.computablefacts.nona.helpers.Text;
 import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
-import com.google.common.base.Splitter;
-import com.google.common.collect.Lists;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
 import com.google.common.collect.Sets;
 import com.google.errorprone.annotations.CheckReturnValue;
 import com.google.errorprone.annotations.Var;
@@ -38,9 +34,13 @@ final public class DocSetLabelerImpl extends DocSetLabeler {
   private final SnowballStemmer stemmer_;
   private final Set<String> stopwords_;
 
-  private IBagOfTexts bagCorpus_ = null;
-  private IBagOfTexts bagSubsetOk_ = null;
-  private IBagOfTexts bagSubsetKo_ = null;
+  private Multiset<String> ngramsCorpus_ = HashMultiset.create();
+  private Multiset<String> ngramsSubsetOk_ = HashMultiset.create();
+  private Multiset<String> ngramsSubsetKo_ = HashMultiset.create();
+
+  private AtomicInteger countCorpus_ = new AtomicInteger(0);
+  private AtomicInteger countSubsetOk_ = new AtomicInteger(0);
+  private AtomicInteger countSubsetKo_ = new AtomicInteger(0);
 
   public DocSetLabelerImpl(Languages.eLanguage language) {
 
@@ -60,47 +60,42 @@ final public class DocSetLabelerImpl extends DocSetLabeler {
   @Override
   protected void init(@NotNull List<String> corpus, @NotNull List<String> subsetOk,
       @NotNull List<String> subsetKo) {
-    if (bagCorpus_ == null) {
+    if (ngramsCorpus_.isEmpty()) {
 
-      AtomicInteger count = new AtomicInteger(0);
       AsciiProgressBar.ProgressBar bar = AsciiProgressBar.create();
-      BagOfTexts bag = new BagOfTexts(Helpers.sentenceSplitter(), Helpers.wordSplitter(language_));
 
-      corpus.stream().peek(
-          t -> bar.update(count.incrementAndGet(), corpus.size(), "Loading the whole corpus..."))
-          .forEach(bag::add);
-
-      bagCorpus_ = new com.computablefacts.morta.docsetlabeler.BagOfTexts(bag.freezeBagOfTexts());
+      corpus.stream().peek(t -> bar.update(countCorpus_.incrementAndGet(), corpus.size(),
+          "Loading the whole corpus...")).forEach(text -> {
+            Set<String> set = new HashSet<>();
+            split(text, set);
+            ngramsCorpus_.addAll(set);
+          });
 
       System.out.println(); // Cosmetic
     }
-    if (bagSubsetOk_ == null) {
+    if (ngramsSubsetOk_.isEmpty()) {
 
-      AtomicInteger count = new AtomicInteger(0);
       AsciiProgressBar.ProgressBar bar = AsciiProgressBar.create();
-      BagOfTexts bag = new BagOfTexts(Helpers.sentenceSplitter(), Helpers.wordSplitter(language_));
 
-      subsetOk.stream()
-          .peek(
-              t -> bar.update(count.incrementAndGet(), subsetOk.size(), "Loading \"OK\" corpus..."))
-          .forEach(bag::add);
-
-      bagSubsetOk_ = new com.computablefacts.morta.docsetlabeler.BagOfTexts(bag.freezeBagOfTexts());
+      subsetOk.stream().peek(t -> bar.update(countSubsetOk_.incrementAndGet(), subsetOk.size(),
+          "Loading \"OK\" corpus...")).forEach(text -> {
+            Set<String> set = new HashSet<>();
+            split(text, set);
+            ngramsSubsetOk_.addAll(set);
+          });
 
       System.out.println(); // Cosmetic
     }
-    if (bagSubsetKo_ == null) {
+    if (ngramsSubsetKo_.isEmpty()) {
 
-      AtomicInteger count = new AtomicInteger(0);
       AsciiProgressBar.ProgressBar bar = AsciiProgressBar.create();
-      BagOfTexts bag = new BagOfTexts(Helpers.sentenceSplitter(), Helpers.wordSplitter(language_));
 
-      subsetKo.stream()
-          .peek(
-              t -> bar.update(count.incrementAndGet(), subsetKo.size(), "Loading \"KO\" corpus..."))
-          .forEach(bag::add);
-
-      bagSubsetKo_ = new com.computablefacts.morta.docsetlabeler.BagOfTexts(bag.freezeBagOfTexts());
+      subsetKo.stream().peek(t -> bar.update(countSubsetKo_.incrementAndGet(), subsetKo.size(),
+          "Loading \"KO\" corpus...")).forEach(text -> {
+            Set<String> set = new HashSet<>();
+            split(text, set);
+            ngramsSubsetKo_.addAll(set);
+          });
 
       System.out.println(); // Cosmetic
     }
@@ -108,58 +103,24 @@ final public class DocSetLabelerImpl extends DocSetLabeler {
 
   @Override
   protected void uinit() {
-    bagSubsetOk_ = null;
-    bagSubsetKo_ = null;
+    ngramsSubsetOk_.clear();
+    ngramsSubsetKo_.clear();
   }
 
   @Override
   protected Set<String> candidates(String text) {
-    return Sets
-        .union(corpusTextUnigrams(text),
-            Sets.union(corpusTextBigrams(text), corpusTextTrigrams(text)))
-        .stream().filter(ngram -> {
 
-          // Discard ngrams with very small words on boundaries
-          return ngram.get(0).length() >= 3 && ngram.get(ngram.size() - 1).length() >= 3;
-        }).filter(ngram -> {
+    Set<String> set = new HashSet<>();
+    split(text, set);
 
-          // Discard ngrams with stopwords on boundaries
-          return !stopwords_.contains(ngram.get(0))
-              && !stopwords_.contains(ngram.get(ngram.size() - 1));
-        }).filter(ngram -> {
-
-          // Discard ngrams that contain numbers
-          return ngram.stream().noneMatch(Strings::isNumber);
-        }).filter(ngram -> {
-
-          // Discard ngrams that do not belong to the OK subset
-          if (ngram.size() == 1) {
-            return bagSubsetOk_.bagOfWords().count(ngram.get(0)) > 0;
-          }
-          if (ngram.size() == 2) {
-            return bagSubsetOk_.bagOfBigrams().count(ngram) > 0;
-          }
-          if (ngram.size() == 3) {
-            return bagSubsetOk_.bagOfTrigrams().count(ngram) > 0;
-          }
-          return false;
-        }).map(ngram -> Joiner.on(' ').join(ngram)).collect(Collectors.toSet());
+    return Sets.intersection(set, ngramsSubsetOk_.elementSet());
   }
 
   @Override
   protected double computeX(String text, String candidate) {
 
-    List<String> words = Splitter.on(' ').trimResults().omitEmptyStrings().splitToList(candidate);
-
-    double freqOk; // the higher, the better
-
-    if (words.size() == 1) {
-      freqOk = bagSubsetOk_.documentFrequency(words.get(0));
-    } else if (words.size() == 2) {
-      freqOk = bagSubsetOk_.documentFrequency(words.get(0), words.get(1));
-    } else { // if (words.size() == 3) {
-      freqOk = bagSubsetOk_.documentFrequency(words.get(0), words.get(1), words.get(2));
-    }
+    // the higher, the better
+    double freqOk = (double) ngramsSubsetOk_.count(candidate) / (double) countSubsetOk_.get();
 
     if (!Double.isFinite(freqOk) || freqOk == 0.0) {
       return 0.0000001;
@@ -170,17 +131,8 @@ final public class DocSetLabelerImpl extends DocSetLabeler {
   @Override
   protected double computeY(String text, String candidate) {
 
-    List<String> words = Splitter.on(' ').trimResults().omitEmptyStrings().splitToList(candidate);
-
-    double freqKo; // the lower, the better
-
-    if (words.size() == 1) {
-      freqKo = bagSubsetKo_.documentFrequency(words.get(0));
-    } else if (words.size() == 2) {
-      freqKo = bagSubsetKo_.documentFrequency(words.get(0), words.get(1));
-    } else { // if (words.size() == 3) {
-      freqKo = bagSubsetKo_.documentFrequency(words.get(0), words.get(1), words.get(2));
-    }
+    // the lower, the better
+    double freqKo = (double) ngramsSubsetKo_.count(candidate) / (double) countSubsetKo_.get();
 
     if (!Double.isFinite(freqKo) || freqKo == 0.0) {
       return 1.0;
@@ -192,130 +144,58 @@ final public class DocSetLabelerImpl extends DocSetLabeler {
   protected List<Map.Entry<String, Double>> filter(
       @NotNull List<Map.Entry<String, Double>> candidates) {
 
-    List<Map.Entry<List<String>, Double>> candidatesNew = candidates.stream()
-        .map(candidate -> new AbstractMap.SimpleEntry<>(
-            Splitter.on(' ').trimResults().omitEmptyStrings().splitToList(candidate.getKey()),
-            candidate.getValue()))
-        .collect(Collectors.toList());
+    // Remove a candidate iif it is a substring of a higher ranked candidate
+    List<Map.Entry<String, Double>> newCandidates = new ArrayList<>();
 
-    List<Map.Entry<List<String>, Double>> candidatesFiltered = new ArrayList<>();
-
-    for (int i = 0; i < candidatesNew.size(); i++) {
-
-      List<String> candidate1 = candidatesNew.get(i).getKey();
-      int middle1 = (candidate1.size() / 2) + 1;
-
-      for (int j = i + 1; j < candidatesNew.size();) {
-
-        List<String> candidate2 = candidatesNew.get(j).getKey();
-        int middle2 = (candidate2.size() / 2) + 1;
-
-        List<List<String>> overlaps1 = overlaps(candidate1, candidate2);
-        List<List<String>> overlaps2 = overlaps(candidate2, candidate1);
-
-        int max1 = overlaps1.stream().mapToInt(List::size).max().orElse(0);
-        int max2 = overlaps2.stream().mapToInt(List::size).max().orElse(0);
-        int max = Math.max(max1, max2);
-
-        if (max >= middle1 || max >= middle2) {
-          candidatesNew.remove(j);
-        } else {
-          j++;
-        }
-      }
-
-      candidatesFiltered.add(candidatesNew.get(i));
-    }
-    return candidatesFiltered.stream()
-        .map(candidate -> new AbstractMap.SimpleEntry<>(Joiner.on(' ').join(candidate.getKey()),
-            candidate.getValue()))
-        .collect(Collectors.toList());
-  }
-
-  /**
-   * Returns all suffixes of list1 that are also a prefix of list2.
-   *
-   * @param list1 first list.
-   * @param list2 second list.
-   * @return overlapping elements.
-   */
-  private List<List<String>> overlaps(List<String> list1, List<String> list2) {
-
-    Preconditions.checkNotNull(list1, "list1 should not be null");
-    Preconditions.checkNotNull(list2, "list2 should not be null");
-
-    List<List<String>> overlaps = new ArrayList<>();
-
-    for (int j = 0; j < list2.size(); j++) {
-
-      if (list1.size() - 1 - j < 0) {
-        return overlaps;
-      }
+    for (int i = 0; i < candidates.size(); i++) {
 
       @Var
-      boolean overlap = true;
-      List<String> suffix = list1.subList(list1.size() - 1 - j, list1.size());
-      List<String> prefix = list2.subList(0, j + 1);
+      boolean match = false;
+      String cur = candidates.get(i).getKey();
 
-      for (int k = 0; k < suffix.size(); k++) {
-        if (!suffix.get(k).equals(prefix.get(k))) {
-          overlap = false;
+      for (int k = 0; k < i; k++) {
+
+        String prev = candidates.get(k).getKey();
+
+        if (prev.contains(cur)) {
+          match = true;
           break;
         }
       }
 
-      if (overlap) {
-        overlaps.add(prefix);
+      if (!match) {
+        newCandidates.add(candidates.get(i));
       }
     }
-    return overlaps;
+    return newCandidates;
   }
 
-  /**
-   * Compute and cache unigrams extracted from a text.
-   *
-   * @param text text.
-   * @return distinct unigrams.
-   */
-  private Set<List<String>> corpusTextUnigrams(String text) {
+  private void split(String text, Set<String> set) {
 
-    Preconditions.checkNotNull(text, "text should not be null");
+    int maxGroupSize = 3;
+    List<List<String>> sentences = Helpers.sentenceSplitter().apply(text).stream()
+        .map(sentence -> Helpers.wordSplitter(language_).apply(sentence))
+        .collect(Collectors.toList());
 
-    return bagCorpus_.text(text).bagOfWords().elementSet().stream()
-        .map(word -> Lists.newArrayList(word)).collect(Collectors.toSet());
-  }
+    for (int i = 0; i < sentences.size(); i++) {
 
-  /**
-   * Compute and cache bigrams extracted from a text.
-   *
-   * @param text text.
-   * @return distinct bigrams.
-   */
-  private Set<List<String>> corpusTextBigrams(String text) {
+      List<String> sentence = sentences.get(i);
 
-    Preconditions.checkNotNull(text, "text should not be null");
+      for (int j = 0; j < sentence.size(); j++) {
+        for (int l = j + 1; l < sentence.size() && l < j + maxGroupSize; l++) {
 
-    Text txt = bagCorpus_.text(text);
+          List<String> words = sentence.subList(j, l);
 
-    return txt.bigrams(null, null, '¤').stream()
-        .map(bigram -> Splitter.on('¤').trimResults().omitEmptyStrings().splitToList(bigram))
-        .collect(Collectors.toSet());
-  }
+          // Discard ngrams with stopwords on boundaries
+          if (stopwords_.contains(words.get(0))
+              || stopwords_.contains(words.get(words.size() - 1))) {
+            continue;
+          }
 
-  /**
-   * Compute and cache trigrams extracted from a text.
-   *
-   * @param text text.
-   * @return distinct trigrams.
-   */
-  private Set<List<String>> corpusTextTrigrams(String text) {
-
-    Preconditions.checkNotNull(text, "text should not be null");
-
-    Text txt = bagCorpus_.text(text);
-
-    return txt.trigrams(null, null, null, '¤').stream()
-        .map(trigram -> Splitter.on('¤').trimResults().omitEmptyStrings().splitToList(trigram))
-        .collect(Collectors.toSet());
+          String ngram = Joiner.on(' ').join(words);
+          set.add(ngram);
+        }
+      }
+    }
   }
 }
