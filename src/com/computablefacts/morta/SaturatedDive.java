efed2a18caaae28f35f813648ab194ae63d39ef3
Cyrille Savelief <csavelief@mncc.fr>
2022-03-06 16:29:20 +0100

    Deal with exceptions

--- src/com/computablefacts/morta/SaturatedDive.java
+++ src/com/computablefacts/morta/SaturatedDive.java
@@ -18,6 +18,7 @@ import com.computablefacts.morta.labelmodels.AbstractLabelModel;
 import com.computablefacts.morta.labelmodels.TreeLabelModel;
 import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
 import com.google.errorprone.annotations.CheckReturnValue;
 
@@ -48,63 +49,70 @@ final public class SaturatedDive extends ConsoleApp {
         .filter(lbl -> label == null || label.equals(lbl)).collect(Collectors.toSet());
 
     for (String lbl : labels) {
+      try {
+        observations.add(
+            "\n================================================================================");
+        observations.add("\nThe label is " + lbl);
+        observations.add("\nBuilding alphabet...");
 
-      observations.add(
-          "\n================================================================================");
-      observations.add("\nThe label is " + lbl);
-      observations.add("\nBuilding alphabet...");
+        Dictionary alphabet = repository.alphabet(lbl);
 
-      Dictionary alphabet = repository.alphabet(lbl);
+        observations.add("\nThe alphabet size is " + alphabet.size());
+        observations.add("\nGuesstimating labeling functions...");
+        observations.add("\nThe number of candidates to consider is " + nbCandidatesToConsider);
+        observations.add("\nThe number of patterns to return is " + nbLabelsToReturn);
 
-      observations.add("\nThe alphabet size is " + alphabet.size());
-      observations.add("\nGuesstimating labeling functions...");
-      observations.add("\nThe number of candidates to consider is " + nbCandidatesToConsider);
-      observations.add("\nThe number of patterns to return is " + nbLabelsToReturn);
+        List<AbstractLabelingFunction<String>> labelingFunctions =
+            repository.labelingFunctions(lbl, nbCandidatesToConsider, nbLabelsToReturn);
 
-      List<AbstractLabelingFunction<String>> labelingFunctions =
-          repository.labelingFunctions(lbl, nbCandidatesToConsider, nbLabelsToReturn);
+        observations.add("\nThe extracted patterns are : [\n  "
+            + Joiner.on(",\n  ").join(labelingFunctions.stream().map(AbstractLabelingFunction::name)
+                .collect(Collectors.toList()))
+            + "\n]");
+        observations.add("\nTraining label model...");
+        observations.add("\nThe evaluation metric is MCC");
 
-      observations
-          .add("\nThe extracted patterns are : [\n  " + Joiner.on(",\n  ").join(labelingFunctions
-              .stream().map(AbstractLabelingFunction::name).collect(Collectors.toList())) + "\n]");
-      observations.add("\nTraining label model...");
-      observations.add("\nThe evaluation metric is MCC");
+        AbstractLabelModel<String> labelModel =
+            repository.labelModel(lbl, labelingFunctions, TreeLabelModel.eMetric.MCC);
 
-      AbstractLabelModel<String> labelModel =
-          repository.labelModel(lbl, labelingFunctions, TreeLabelModel.eMetric.MCC);
+        observations.add("\nThe label model is " + labelModel.toString());
+        observations.add("\nTraining classifier...");
+        observations.add("\nThe classifier type is LOGIT");
 
-      observations.add("\nThe label model is " + labelModel.toString());
-      observations.add("\nTraining classifier...");
-      observations.add("\nThe classifier type is LOGIT");
+        AbstractClassifier classifier =
+            repository.classifier(lbl, alphabet, labelModel, Repository.eClassifier.LOGIT);
+        // TODO : save prodigy annotations
 
-      AbstractClassifier classifier =
-          repository.classifier(lbl, alphabet, labelModel, Repository.eClassifier.LOGIT);
-      // TODO : save prodigy annotations
+        observations.add("\nSummarizing label model...");
 
-      observations.add("\nSummarizing label model...");
+        labelModel.summarize(Lists.newArrayList(repository.pagesAsGoldLabels(lbl)))
+            .forEach(System.out::println);
 
-      labelModel.summarize(Lists.newArrayList(repository.pagesAsGoldLabels(lbl)))
-          .forEach(System.out::println);
+        List<IGoldLabel<String>> labelModelPredictions = repository.pagesAsGoldLabels(lbl).stream()
+            .map(goldLabel -> newGoldLabel(goldLabel,
+                labelModel.predict(Lists.newArrayList(goldLabel)).get(0)))
+            .collect(Collectors.toList());
 
-      List<IGoldLabel<String>> labelModelPredictions = repository.pagesAsGoldLabels(lbl).stream()
-          .map(goldLabel -> newGoldLabel(goldLabel,
-              labelModel.predict(Lists.newArrayList(goldLabel)).get(0)))
-          .collect(Collectors.toList());
+        ConfusionMatrix labelModelConfusionMatrix =
+            IGoldLabel.confusionMatrix(labelModelPredictions);
 
-      ConfusionMatrix labelModelConfusionMatrix = IGoldLabel.confusionMatrix(labelModelPredictions);
+        observations.add("\nComputing the label model confusion matrix...");
+        observations.add(labelModelConfusionMatrix.toString());
 
-      observations.add("\nComputing the label model confusion matrix...");
-      observations.add(labelModelConfusionMatrix.toString());
+        List<IGoldLabel<String>> classifierPredictions = repository.pagesAsGoldLabels(lbl).stream()
+            .map(goldLabel -> newGoldLabel(goldLabel,
+                repository.classify(alphabet, classifier, goldLabel.data())))
+            .collect(Collectors.toList());
 
-      List<IGoldLabel<String>> classifierPredictions = repository.pagesAsGoldLabels(lbl).stream()
-          .map(goldLabel -> newGoldLabel(goldLabel,
-              repository.classify(alphabet, classifier, goldLabel.data())))
-          .collect(Collectors.toList());
+        ConfusionMatrix classifierConfusionMatrix =
+            IGoldLabel.confusionMatrix(classifierPredictions);
 
-      ConfusionMatrix classifierConfusionMatrix = IGoldLabel.confusionMatrix(classifierPredictions);
+        observations.add("\nComputing the classifier confusion matrix...");
+        observations.add(classifierConfusionMatrix.toString());
 
-      observations.add("\nComputing the classifier confusion matrix...");
-      observations.add(classifierConfusionMatrix.toString());
+      } catch (Exception e) {
+        observations.add(Throwables.getStackTraceAsString(Throwables.getRootCause(e)));
+      }
     }
 
     observations.flush();
